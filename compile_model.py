25af3c3f57d17bff9446c582aed8f16b2e0a052c
Juliana Peixoto <alencar.julianapeixoto@gmail.com>
2021-02-13 14:03:10 -0300

    stan linear model

---
+++ compile_model.py
@@ -0,0 +1,14 @@
+import sys
+import pickle
+
+st_code = sys.argv[1]
+st_obj = sys.argv[2]
+
+sm = pystan.StanModel(file=st_code)
+
+with open(st_obj, "wb") as f:
+    pickle.dump(sm, f)
+
+print("done")
+
+
